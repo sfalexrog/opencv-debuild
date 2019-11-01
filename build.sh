a09f4874bfd974d45599ec14f4d48a36abed88df
Alexey Rogachevskiy <sfalexrog@gmail.com>
2019-11-01 16:48:14 +0300

    build: Fetch sources automatically

--- build.sh
+++ build.sh
@@ -5,12 +5,22 @@
 
 echo "--- Installing base packages"
 apt update
-apt install -y build-essential devscripts fakeroot
+apt install -y build-essential devscripts fakeroot ninja-build
+
+echo "--- Getting opencv sources"
+git clone https://github.com/opencv/opencv -b 3.4.8 --depth 1 deb_opencv_clever
+cd deb_opencv_clever
+git clone https://github.com/opencv/opencv_contrib -b 3.4.8 --depth 1 contrib
+cd ..
+
+echo "--- Copying Debian build rules"
+cp -r debian deb_opencv_clever/
 
 echo "--- Installing dependencies"
 cd deb_opencv_clever
 yes | mk-build-deps -i
 
 echo "--- Building packages"
+export DEB_BUILD_OPTIONS="parallel=12"
 fakeroot debian/rules binary
 
